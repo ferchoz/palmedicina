e6e90293aae4bf5366fd0ac44398a5bae618d547
frivas@silicon-systems.com.ar <frivas@silicon-systems.com.ar@4a8ae1eb-97e3-0b9a-6f64-5a2e7538e6b9>
2015-03-04 11:19:29 +0000

    add sonata/media

---
+++ src/AppBundle/Admin/SlideCompanyAdmin.php
@@ -0,0 +1,33 @@
+<?php
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class SlideCompanyAdmin extends Admin
+{
+    protected $baseRouteName    = 'admin_slide_company';
+    protected $baseRoutePattern = 'slide-company';
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Post Title'))
+            ->add('image', 'sonata_type_model_list', array(), array('link_parameters' => array('context' => 'slider_company')));
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('title');
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('title');
+    }
+}
